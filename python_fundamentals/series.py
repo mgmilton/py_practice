0e1eddafc5e77d4d85ac65dcd22d30d6644da676
mgmilton <matt.milton@gmail.com>
2018-06-26 16:30:07 -0700

    adds series creation

---
+++ python_fundamentals/series.py
@@ -0,0 +1,38 @@
+import pandas as pd
+animals = ['Tiger', 'Bear', 'Moose']
+pd.Series(animals)
+
+numbers = [1, 2, 3]
+pd.Series(numbers)
+
+animals = ['Tiger', 'Bear', None]
+pd.Series(numbers)
+
+numbers = [1, 2, None]
+pd.Series(numbers)
+
+import numpy as np
+np.nan == None
+
+np.nan == np.nan
+
+np.isnan(np.nan)
+
+sports = {'Archery': 'Bhutan',
+         'Golf': 'Scotland',
+         'Sumo': 'Japan',
+         'Taekwondo': 'South Korea'}
+s = pd.Series(sports)
+print(s)
+print(s.index)
+
+s = pd.Series(['Tiger', 'Bear', 'Moose'], index=['India', 'America', 'Canada'])
+print(s)
+print(s.index)
+
+sports = {'Archery': 'Bhutan',
+          'Golf': 'Scotland',
+          'Sumo': 'Japan',
+          'Taekwondo': 'South Korea'}
+s = pd.Series(sports, index=['Golf', 'Sumo', 'Hockey'])
+print(s)
