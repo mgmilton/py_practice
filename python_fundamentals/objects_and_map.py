fcfc333716dae14e117ead64c036b5ed353f5dca
mgmilton <matt.milton@gmail.com>
2018-06-26 14:06:21 -0700

    adds map function

--- python_fundamentals/objects_and_map.py
+++ python_fundamentals/objects_and_map.py
@@ -15,3 +15,13 @@ person = Person()
 person.set_name('Barack Obama')
 person.set_location('Hyde Park Chicago, IL, USA')
 print('{} lives in {} and works as a {}.'.format(person.name, person.location, person.job))
+
+
+#an example of mapping the min function between two lists
+store1 = [10.00, 11.00, 12.34, 2.34]
+store2 = [9.00, 11.10, 12.34, 2.01]
+cheapest = map(min, store1, store2)
+cheapest
+
+for item in cheapest:
+    print(item)
