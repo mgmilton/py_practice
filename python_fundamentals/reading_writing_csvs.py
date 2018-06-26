7ea562f5467adbadcc09658c50cdef9285e98354
mgmilton <matt.milton@gmail.com>
2018-06-26 13:57:21 -0700

    adds csv looping function

--- python_fundamentals/reading_writing_csvs.py
+++ python_fundamentals/reading_writing_csvs.py
@@ -53,3 +53,15 @@ vehicleclass = set(d['class'] for d in mpg) #what are the class types
 vehicleclass
 
 # How to find the average hwy mpg for each class of vehicle in our dataset
+HwyMpgByClass = []
+for t in vehicleclass: # iterate over all the vehicle classes
+    summpg = 0
+    vclasscount = 0
+    for d in mpg: # iterate over all dictionaries
+        if d['class'] == t: # if the cylinder amount type matches,
+            summpg += float(d['hwy']) # add the hwy mpg
+            vclasscount += 1 # increment the count
+    HwyMpgByClass.append((t, summpg / vclasscount)) # append the tuple ('class', 'avg mpg')
+
+HwyMpgByClass.sort(key=lambda x: x[1])
+print(HwyMpgByClass)
