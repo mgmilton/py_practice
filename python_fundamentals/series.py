b92ed331280c0f7a9176b650c378339c654157ac
mgmilton <matt.milton@gmail.com>
2018-06-27 10:36:20 -0700

    Adds series querying

--- python_fundamentals/series.py
+++ python_fundamentals/series.py
@@ -53,3 +53,64 @@ s.loc['Golf'] # returns Scotland
 s[3] #returns South Korea
 
 s['Golf'] # returns Scotland
+
+s[0] #This won't call s.iloc[0] as one might expect, it generates an error instead
+
+s = pd.Series([100.00, 120.00, 101.00, 3.00])
+s
+
+total = 0
+for item in s:
+    total += item
+print(total)
+
+#using vectorization
+
+total = np.sum(s)
+print(total)
+
+
+#this creates a big series of random numbers
+s = pd.Series(np.random.randint(0,1000,10000))
+s.head()
+len(s)
+
+summary = 0
+for item in s:
+    summary+=item
+
+summary = np.sum(s)
+
+s+=2
+s.head()
+
+for label, value in s.iteritems():
+    s.set_value(label, value+2)
+print(s.head())
+
+s = pd.Series(np.random.randint(0,1000,10000))
+for label, value in s.iteritems():
+    s.loc[label] = value +2
+
+#faster with vectorization
+s = pd.Series(np.random.randint(0,1000,10000))
+s+=2
+
+original_sports = pd.Series({'Archery': 'Bhutan',
+                             'Golf': 'Scotland',
+                             'Sumo': 'Japan',
+                             'Taekwondo': 'South Korea'})
+cricket_loving_countries = pd.Series(['Australia',
+                                      'Barbados',
+                                      'Pakistan',
+                                      'England'],
+                                   index=['Cricket',
+                                          'Cricket',
+                                          'Cricket',
+                                          'Cricket'])
+all_countries = original_sports.append(cricket_loving_countries)
+
+print(original_sports)
+print(cricket_loving_countries)
+print(all_countries)
+print(all_countries.loc['Cricket'])
