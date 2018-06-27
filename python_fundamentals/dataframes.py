c5e6fb5a342519485c712e03faaebdadfc35aa8d
mgmilton <matt.milton@gmail.com>
2018-06-27 12:23:11 -0700

    adds olympics dataframe

--- python_fundamentals/dataframes.py
+++ python_fundamentals/dataframes.py
@@ -22,6 +22,48 @@ df.T.loc['Cost']
 df['Cost']
 df.loc['Store 1']['Cost']
 
-print(df)
 # to query by column name
-print(df.loc[:,['Name', 'Cost']])
+df.loc[:,['Name', 'Cost']]
+
+
+# Dataframe Indexing and Loading
+
+costs = df['Cost']
+
+costs += 2
+costs
+
+df = pd.read_csv('./course1_downloads/olympics.csv', index_col=0, skiprows=1)
+df.head()
+df.columns
+
+for col in df.columns:
+    if col[:2]=='01':
+        df.rename(columns={col:'Gold' + col[4:]}, inplace=True)
+    if col[:2]=='02':
+        df.rename(columns={col:'Silver' + col[4:]}, inplace=True)
+    if col[:2]=='03':
+        df.rename(columns={col:'Bronze' + col[4:]}, inplace=True)
+    if col[:1]=='№':
+        df.rename(columns={col:'#' + col[1:]}, inplace=True)
+
+df.head()
+
+
+# Querying a DataFrame
+
+df['Gold'] > 0 # returns all Countries with boolean if the country has won a gold medal
+
+only_gold = df.where(df['Gold'] > 0)
+only_gold.head()
+
+only_gold['Gold'].count()
+df['Gold'].count()
+only_gold = only_gold.dropna()
+only_gold.head()
+
+only_gold = df[df['Gold'] > 0]
+only_gold.head()
+
+print(len(df[(df['Gold'] > 0) | (df['Gold.1'] > 0)]))
+print(len(df[(df['Gold.1'] > 0) | (df['Gold'] == 0)]))
